a439cefab2c3803a94fecb9f64f169f02dd9f5d4
최지수 <cjscoding1997@gmail.com>
2022-04-05 23:10:32 +0900

    최지수 0405

--- 알고리즘스터디/jisu/programmers/Lv.2/문자열 압축.js
+++ 알고리즘스터디/jisu/programmers/Lv.2/문자열 압축.js
@@ -1,35 +1,35 @@
-// const solution = (s) => {
-//   let min = s.length;
+const solution = (s) => {
+  let total = s.length;
+  let answer = 1001;
+  let temp;
+  for (let l = 1; l <= total; l++) {
+    temp = "";
+    for (let i = 0; i < s.length; i++) {
+      let info = str(l, i);
+      temp += info.temp;
+      i = info.i - 1;
+    }
+    answer = Math.min(answer, temp.length);
+  }
+  return answer;
 
-//   for (let i = 2; i < s / 2; i++) {
-//     let made = "";
-//     let same = 1;
-//     let pat = "";
-//     let start = 0;
-//     let end = i;
-//     out: while (true) {
-//       let tmp = "";
-//       for (let j = start; j < end; j++) {
-//         if (j === s.length) {
-//           made += tmp;
-//           break out;
-//         }
-//         tmp += s.charAt(j);
-//       }
+  const num = (num) => {
+    return num == 1 ? "" : num;
+  };
+  
+  const str = (l, cur) => {
+    let i = cur + l;
+    let temp = "";
+    if (cur + l <= total) {
+      let cnt = 1;
+      let target = s.slice(cur, cur + l);
 
-//       if (tmp === pat) {
-//         same++;
-//       } else {
-//         made += same;
-//         made += pat;
-//         same = 1;
-//         pat = "";
-//       }
-
-//       start += i;
-//       end += i;
-//     }
-
-//     min = Math.min(made.length, min);
-//   }
-// };
+      for (i = cur + l; i < total; i += l) {
+        if (target == s.slice(i, i + l)) cnt++;
+        else break;
+      }
+      temp += num(cnt) + target;
+    } else temp = s.slice(cur, total);
+    return { temp: temp, i: i };
+  };
+};

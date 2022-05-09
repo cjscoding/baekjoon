f8718ef55df93298ad12308bff74af9b3092e95b
김혜인 <fallingstars2749@gmail.com>
2022-05-09 22:59:08 +0900

    220509 혜인

---
+++ 알고리즘스터디/hyein/programmers/서울에서김서방찾기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String[] seoul) {
+        int position = -1;
+        for(int i = 0; i < seoul.length; i++) {
+            if(seoul[i].equals("Kim")) {
+                position = i;
+                break;
+            }
+        }
+        
+        String answer = "김서방은 " + position + "에 있다";
+        return answer;
+    }
+}
